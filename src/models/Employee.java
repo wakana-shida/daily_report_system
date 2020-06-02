4571e5cc224c05f8d32a62a0b466709965f5546c
wakana <shima_shima_sw@outlook.jp>
2020-06-02 16:23:30 +0900

    Add login / logout

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -26,8 +26,8 @@ import javax.persistence.Table;
             query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
             ),
     @NamedQuery (
-            name = "checkLoginCodePassword",
-            query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+            name = "checkLoginCodeAndPassword",
+            query ="SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
             )
 } )
 @Entity
